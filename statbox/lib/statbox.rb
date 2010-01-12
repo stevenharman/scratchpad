85559462937d43e38cc1002cc7c75cb96512fd24
Steven Harman <steveharman@gmail.com>
2010-01-12 11:50:41 -0500

    moving the simple statbox stuff out to its own repo for sharing

--- statbox/lib/statbox.rb
+++
@@ -1,4 +0,0 @@
-$: << File.expand_path(File.dirname(__FILE__))
-$: << File.expand_path(File.join(File.dirname(__FILE__), "statbox"))
-
-Dir.glob(File.join(File.expand_path(File.dirname(__FILE__)), 'statbox/*.rb')).each {|f| require f }
